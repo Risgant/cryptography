8206a4e2e42c6c38385be8b825e246a9061a8e46
Risgant <dshnitko@inbox.ru>
2021-11-05 08:15:58 +0300

    init db

--- Lab2/server/src/main/resources/database/V1.01_init.sql
+++ Lab2/server/src/main/resources/database/V1.01_init.sql
@@ -1,17 +1,21 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+DROP TABLE IF EXISTS note;
+DROP TABLE IF EXISTS app_user;
+
 CREATE TABLE app_user (
-    id uuid NOT NULL DEFAULT uuid_generate_v1(),
-    name varchar NOT NULL,
-    password varchar NOT NULL,
-    CONSTRAINT app_user_pkey PRIMARY KEY (id),
-    CONSTRAINT app_user_uniq UNIQUE ("name")
+                          id uuid NOT NULL DEFAULT uuid_generate_v1(),
+                          name varchar NOT NULL,
+                          password varchar NOT NULL,
+                          CONSTRAINT app_user_pkey PRIMARY KEY (id),
+                          CONSTRAINT app_user_uniq UNIQUE ("name")
 );
 
 CREATE TABLE note (
-    id uuid NOT NULL DEFAULT uuid_generate_v1(),
-    user_id uuid NOT NULL,
-    text varchar NOT NULL,
-    CONSTRAINT note_pkey PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES app_user (id)
+                      id uuid NOT NULL DEFAULT uuid_generate_v1(),
+                      user_id uuid NOT NULL,
+                      title varchar NOT NULL,
+                      text varchar NOT NULL,
+                      CONSTRAINT note_pkey PRIMARY KEY (id),
+                      FOREIGN KEY (user_id) REFERENCES app_user (id)
 );
